874531758e6de7c926b3146a07e465b1752187c2
David Acosta <david@rocksteady.technology>
2022-12-30 20:14:19 -0800

    Pass simple flags correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ echo" Running yatt-pipe: /yatt-pipe/node_modules/yatt-pipe/index.js \
  ${INPUT_IGNORE:+--ignore $INPUT_IGNORE} \
  ${INPUT_OVERRIDE:+--overrides $INPUT_OVERRIDE} \
  ${INPUT_DATA_TYPES:+--data-types $INPUT_DATA_TYPES} \
- ${INPUT_NO_GIT:+--no-git $INPUT_NO_GIT} \
- ${INPUT_VERBOSE:+--verbose $INPUT_VERBOSE}"
+ ${INPUT_NO_GIT:+--no-git} \
+ ${INPUT_VERBOSE:+--verbose}"
 
 node /yatt-pipe/node_modules/yatt-pipe/index.js \
  ${INPUT_SOURCE:+--source $INPUT_SOURCE} \
@@ -17,5 +17,5 @@ node /yatt-pipe/node_modules/yatt-pipe/index.js \
  ${INPUT_IGNORE:+--ignore $INPUT_IGNORE} \
  ${INPUT_OVERRIDE:+--overrides $INPUT_OVERRIDE} \
  ${INPUT_DATA_TYPES:+--data-types $INPUT_DATA_TYPES} \
- ${INPUT_NO_GIT:+--no-git $INPUT_NO_GIT} \
- ${INPUT_VERBOSE:+--verbose $INPUT_VERBOSE}
+ ${INPUT_NO_GIT:+--no-git} \
+ ${INPUT_VERBOSE:+--verbose}
