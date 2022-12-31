27044072633770ff64fa6a65f9c5fbb1795058d4
David Acosta <david@rocksteady.technology>
2022-12-30 23:56:02 -0800

    Actually apply newest changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -60,7 +60,7 @@ node /yatt-pipe/node_modules/yatt-pipe/index.js \
  ${INPUT_TARGET:+--target $INPUT_TARGET} \
  ${INPUT_CREDENTIALS:+--credentials $INPUT_CREDENTIALS} \
  ${INPUT_IGNORE:+--ignore $INPUT_IGNORE} \
- ${INPUT_OVERRIDE:+--overrides $INPUT_OVERRIDE} \
+ ${COMBINED_OVERRIDE:+--overrides \"$COMBINED_OVERRIDE\"} \
  ${INPUT_DATA_TYPES:+--data-types $INPUT_DATA_TYPES} \
  ${INPUT_NO_GIT:+--no-git} \
  ${INPUT_VERBOSE:+--verbose}
